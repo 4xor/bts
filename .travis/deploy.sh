7a072557eb55ff8f8b10e1f26161e768475f5efc
4xor <me@4xor.io>
2018-10-30 06:37:13 +0300

    compile css before deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+stylus stylus/admin/index.styl -o resources/public/css/admin.css
+stylus stylus/bts/index.styl -o resources/public/css/main.css
 lein uberjar
 lein docker-publish
