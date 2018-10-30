410e910500b368701624045137461294fd32792d
4xor <me@4xor.io>
2018-10-30 06:03:17 +0300

    Add bind host name env param

--- src/clj/bts/core.clj
+++ src/clj/bts/core.clj
@@ -5,9 +5,11 @@
             [aleph.http :as http]
             [bts.web :as web]
             [bts.components.scheduler :as scheduler])
-  (:gen-class))
+  (:gen-class)
+  (:import (java.net InetSocketAddress)))
 
 (def port (env :port "8080"))
+(def host (env :host "127.0.0.1"))
 (def twc (Integer/parseInt (env :count-topic-workers "1")))
 (def enable-scheduler (= (env :enable-scheduler "true") "true"))
 
@@ -15,4 +17,4 @@
   (migrate)
   (when enable-scheduler (scheduler/start db))
   (dotimes [_ twc] (rt/start-topic-worker db))
-  (http/start-server web/handler {:port (Integer/parseInt port)}))
+  (http/start-server web/handler {:socket-address (InetSocketAddress. host (Integer/parseInt port))}))
