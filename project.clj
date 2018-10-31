118d8482b713981d2bb321ba90382fa29007f126
4xor <me@4xor.io>
2018-10-31 07:08:11 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bts "0.1.1"
+(defproject bts "0.1.2-SNAPSHOT"
   :description "BitTorrent parse and search with tags and metadata"
   :url "http://bts.4xor.io"
   :license {:name "Eclipse Public License"
