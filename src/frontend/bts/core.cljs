7b8eacd36f00dc185fcc28d6fe899462322233c6
4xor <me@4xor.io>
2018-10-31 04:38:53 +0300

    ui bug fix

--- src/frontend/bts/core.cljs
+++ src/frontend/bts/core.cljs
@@ -2,9 +2,9 @@
   (:require [reagent.core :as r]
             [bts.components.index :as index]))
 
-(defn ^:export  main []
+(defn ^:export main []
   (when-not (= (.-hostname js/location) "localhost")
-    (when-not (= (.-protocol js/location) "https") (set! (.-protocol js/location) "https")))
+    (when-not (= (.-protocol js/location) "https:") (set! (.-protocol js/location) "https:")))
   (r/render [index/component]
             (.getElementById js/document "app")))
 
